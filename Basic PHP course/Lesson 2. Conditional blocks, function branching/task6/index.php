31b6f6d41ceae759b0e0187cdc29b8468e484a72
SergeyShashin <s9997910@yandex.ru>
2023-09-09 00:02:52 +0300

    Lesson2. Task6 done.

--- Basic PHP course/Lesson 2. Conditional blocks, function branching/task6/index.php
+++ Basic PHP course/Lesson 2. Conditional blocks, function branching/task6/index.php
@@ -2,17 +2,14 @@
 // 6. *С помощью рекурсии организовать функцию возведения числа в степень.
 //  Формат: function power($val, $pow), где $val – заданное число, $pow – степень.
 
-echo power(2, 3);
+echo power(2, 8);
 
 function power($val, $pow)
 {
   if ($pow == 1) {
     return $val*1;
   } else {
-    $res = $val * $val;
     $pow = $pow - 1;
-    // echo $pow . "\n";
-    // echo $val;
-    return power($res, $pow);
+    return $val*power($val, $pow);
   }
 }
