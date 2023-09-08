15061e048b4d5142df04317d99115df6d7d518bd
SergeyShashin <s9997910@yandex.ru>
2023-09-08 16:42:08 +0300

    Lesson2. Task5 done.

---
+++ Basic PHP course/Lesson 2. Conditional blocks, function branching/task5/index.php
@@ -0,0 +1,18 @@
+<!-- 5. Посмотреть на встроенные функции PHP.
+ Используя имеющийся HTML-шаблон, вывести текущий год в подвале при помощи встроенных функций PHP. -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h1>Текущий год <?php echo date('Y') ?></h1>
+
+</body>
+
+</html>
