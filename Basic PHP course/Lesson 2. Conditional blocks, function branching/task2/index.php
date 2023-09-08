c778d86202f4d701caeda7c5cbba0ddf534f81c1
SergeyShashin <s9997910@yandex.ru>
2023-09-08 16:08:19 +0300

    Lesson2. Task2 done.

---
+++ Basic PHP course/Lesson 2. Conditional blocks, function branching/task2/index.php
@@ -0,0 +1,40 @@
+<?php
+//2. Присвоить переменной $а значение в промежутке [0..15].
+//С помощью оператора switch организовать вывод чисел от $a до 15.
+
+$a = 8;
+
+switch ($a) {
+  case 0:
+    echo" 0\n";
+  case 1:
+    echo" 1\n";
+  case 2:
+    echo" 2\n";
+  case 3:
+    echo" 3\n";
+  case 4:
+    echo" 4\n";
+  case 5:
+    echo" 5\n";
+  case 6:
+    echo" 6\n";
+  case 7:
+    echo" 7\n";
+  case 8:
+    echo" 8\n";
+  case 9:
+    echo" 9\n";
+  case 10:
+    echo "10\n";
+  case 11:
+    echo "11\n";
+  case 12:
+    echo "12\n";
+  case 13:
+    echo "13\n";
+  case 14:
+    echo "14\n";
+  case 15:
+    echo "15\n";
+}
