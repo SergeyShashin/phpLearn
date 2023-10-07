418fe5cefa731904ef3778209268925faa7e9161
SergeyShashin <s9997910@yandex.ru>
2023-10-07 18:01:30 +0300

    Lesson8 update.

--- Basic PHP course/Lesson 8. Practice/geek-project/public/user.php
+++ Basic PHP course/Lesson 8. Practice/geek-project/public/user.php
@@ -10,25 +10,39 @@ function routeIndex()
 //страница с формой входа
 function routeLogin()
 {
-  //редирект, если авторизован
-  if (isLoggedUser()) {
-    header('Location: \user.php?action=home');
-  }
-
-  //проверка данных из формы
-  if (isset($_POST['signIn'])) {
-    systemLog($_POST, 'debug');
-    $login = $_POST['login'];
-    $password = $_POST['password'];
-
-    if (isset($_POST['remember'])) {
-      loginUser($login, $password, true);
-    } else {
-      loginUser($login, $password);
-    }
-    header('Location: /user.php?action=home');
-  }
   echo render('user/signIn');
+
+  //редирект, если авторизован
+  // if (isLoggedUser()) {
+  //   header('Location: \user.php?action=home');
+  // }
+
+  // //проверка данных из формы
+  // if (isset($_POST['signIn'])) {
+  //   systemLog($_POST, 'debug');
+  //   $login = $_POST['login'];
+  //   $password = $_POST['password'];
+
+  //   if (isset($_POST['remember'])) {
+  //     loginUser($login, $password, true);
+  //   } else {
+  //     loginUser($login, $password);
+  //   }
+  //   header('Location: /user.php?action=home');
+  // }
+
+  echo "Выполнение routeLogin()";
+  echo "Содержимое GET";
+  print_r($_GET);
+
+  echo "Содержимое $_POST";
+  print_r($_POST);
+
+  echo "Содержимое SESSION";
+  print_r($_SESSION);
+
+  echo "Содержимое COOKIE";
+  print_r($_COOKIE);
 }
 
 function routeLogout()
@@ -43,28 +57,68 @@ function routeHome()
 
 function routeRegister()
 {
-  //грузим из POST
-  if (isset($_POST['reg_user'])) {
+  echo render('user/register');
+
+  // //грузим из POST
+  // if (isset($_POST['reg_user'])) {
+  //   $login = $_POST['login'];
+  //   $password = $_POST['password'];
+
+  //   //хешируем пароль
+  //   $password = password_hash($password, PASSWORD_DEFAULT);
+
+  //   //готовим запрос
+  //   $sql = "INSERT into users (login, password) values ('{$login}', '{$password}')";
+
+
+  //   if (execute($sql)) {
+  //     //авторизуем
+
+  //     loginUser($login, $password, true);
+  //     header('Location: /user.php?action=home');
+  //   }
+  // }
+  echo "Выполнение routeRegister()";
+  echo "Содержимое GET";
+  print_r($_GET);
+
+  echo "Содержимое $_POST";
+  print_r($_POST);
+
+  echo "Содержимое SESSION";
+  print_r($_SESSION);
+
+  echo "Содержимое COOKIE";
+  print_r($_COOKIE);
+
+  //Сделать
+  //Добавить в БД логин и пароль
+  if (isset($_POST['reg_user']) && isset($_POST['login']) && isset($_POST['password']) && strlen($_POST['login']) && strlen($_POST['password'])) {
     $login = $_POST['login'];
-    $password = $_POST['password'];
-
-    //хешируем пароль
-    $password = password_hash($password, PASSWORD_DEFAULT);
-
-    //готовим запрос
-    $sql = "INSERT into users (login, password) values ('{$login}', '{$password}')";
-
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $sql = "INSERT into `users` (`login`, `password`) values ('{$login}', '{$password}')";
 
     if (execute($sql)) {
-      //авторизуем
+      //авторизовать пользователя (добавить в сессию, куки информацию, установить роль гость, авторизованный или админ)
+      //Подгрузить страницу user/home
+      // header('Location: /user.php?action=home');
+    };
 
-      loginUser($login, $password, true);
-      header('Location: /user.php?action=home');
-    }
+    $sql = "SELECT password from `users` WHERE(`login`= '{$login}')";
+
+    $getPasswordFromBd = getItem($sql);
+
+    $passwordFromBd = $getPasswordFromBd['password'];
+
+    var_dump(password_verify('123', $passwordFromBd) );
+
   }
 
-  echo render('user/register');
 }
 
+//Обрабатывает редактирование формы на странице user/home
+function routeApprove()
+{
+}
 
 route();
--- Basic PHP course/Lesson 8. Practice/geek-project/templates/user/home.php
+++ Basic PHP course/Lesson 8. Practice/geek-project/templates/user/home.php
@@ -1,8 +1,36 @@
-<h1><?php echo "Добро пожаловать" ?></h1>
+<h1>"Добро пожаловать"<?php echo 'Логин пользователя' ?></h1>
 
-<h2><?php var_dump($_SESSION)?></h2>
-
+<h2></h2>
 <p>
   Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure unde optio ea ducimus recusandae neque,
   magnam tempore dolore illum maxime obcaecati accusantium eos sapiente nostrum rem perspiciatis veniam eius culpa.
 </p>
+<div class="container col-3">
+  <form method="post">
+    <div class="mb-3">
+      <label for="login" class="form-label">Логин</label>
+      <input type="text" class="form-control" id="login" name='login' placeholder="Вывести текущий ЛОГИН из БД">
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label" require>Пароль</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" name='password' placeholder="ВЫВЕСТИ текущий ПАРОЛЬ из БД">
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label">Имя</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" name='first_name' placeholder="Вывести Имя пользователя из БД или ничего, если в БД нет">
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label">Фамилия</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" name='last_name' placeholder="Фамилию пользователя из БД или ничего, если в БД нет">
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label">Отчество</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" name='middle_name' placeholder="Отчество пользователя из БД или ничего, если в БД нет">
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label">Возраст</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" name='age' placeholder="Возраст пользователя из БД или ничего, если в БД нет">
+    </div>
+    <a href="/user.php?action=approve&id=<?php echo 'взять из БД' ?>" type="submit" class="btn btn-primary">Утвердить</button>
+  </form>
+</div>
