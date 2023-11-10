09f1c045cf04bcf6d06e2510a53b666d08e237ff
SergeyShashin <s9997910@yandex.ru>
2023-11-10 13:28:19 +0300

    Professional web development in PHP. Lesson 3. Template engines. Update.

---
+++ Professional web development in PHP/Lesson 3. Template engines/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use App\View\Page;
+
+$page = new Page();
+
+var_dump($page);
--- Professional web development in PHP/Lesson 3. Template engines/vendor/composer/autoload_static.php
+++ Professional web development in PHP/Lesson 3. Template engines/vendor/composer/autoload_static.php
@@ -21,6 +21,10 @@ class ComposerStaticInite354ea5f99a439d8e4b390249319c83f
             'Symfony\\Polyfill\\Mbstring\\' => 26,
             'Symfony\\Polyfill\\Ctype\\' => 23,
         ),
+        'A' => 
+        array (
+            'App\\' => 4,
+        ),
     );
 
     public static $prefixDirsPsr4 = array (
@@ -35,6 +39,10 @@ class ComposerStaticInite354ea5f99a439d8e4b390249319c83f
         'Symfony\\Polyfill\\Ctype\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/polyfill-ctype',
+        ),
+        'App\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/app',
         ),
     );
 
